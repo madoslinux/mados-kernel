469e5e6db2e71ae8fc2f7de73e59c201e2c530bc
madkoding <madkoding@gmail.com>
2026-04-09 20:04:05 -0400

    feat: expand hardware support and untrack build artifacts

--- scripts/validate-kernel-config.sh
+++ scripts/validate-kernel-config.sh
@@ -41,4 +41,15 @@ check_cfg '^CONFIG_SQUASHFS=y$' 'CONFIG_SQUASHFS must be built-in for live media
 check_cfg '^CONFIG_ZSMALLOC=m$' 'CONFIG_ZSMALLOC should be enabled as module for zram support'
 check_cfg '^CONFIG_ZRAM=m$' 'CONFIG_ZRAM should be enabled as module'
 
+check_cfg '^CONFIG_CFG80211=m$' 'CONFIG_CFG80211 should be enabled as module for Wi-Fi stack support'
+check_cfg '^CONFIG_MAC80211=m$' 'CONFIG_MAC80211 should be enabled as module for Wi-Fi stack support'
+check_cfg '^CONFIG_RTW88_CORE=m$' 'CONFIG_RTW88_CORE must be enabled as module for Realtek rtw88'
+check_cfg '^CONFIG_RTW88_PCI=m$' 'CONFIG_RTW88_PCI must be enabled as module for Realtek rtw88 PCI adapters'
+check_cfg '^CONFIG_RTW88_8723DE=m$' 'CONFIG_RTW88_8723DE must be enabled as module for Realtek 8723DE support'
+check_cfg '^CONFIG_RTW89_CORE=m$' 'CONFIG_RTW89_CORE should be enabled as module for modern Realtek support'
+check_cfg '^CONFIG_ATH10K_PCI=m$' 'CONFIG_ATH10K_PCI should be enabled as module for common Atheros PCI adapters'
+check_cfg '^CONFIG_ATH11K_PCI=m$' 'CONFIG_ATH11K_PCI should be enabled as module for common Wi-Fi 6 Atheros PCI adapters'
+check_cfg '^CONFIG_BRCMFMAC_PCIE=y$' 'CONFIG_BRCMFMAC_PCIE should be enabled for Broadcom PCIe FullMAC adapters'
+check_cfg '^CONFIG_MT7921E=m$' 'CONFIG_MT7921E should be enabled as module for common MediaTek PCIe adapters'
+
 printf 'Kernel config validation passed: %s\n' "$cfg_file"
